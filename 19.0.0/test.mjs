7136e95f2d19259254ccc212043750b00524fd8e
Cody Olsen <stipsan@gmail.com>
2025-01-14 23:52:03 +0100

    add 19 baseline

---
+++ 19.0.0/test.mjs
@@ -0,0 +1,4 @@
+import assert from 'node:assert'
+
+import { c } from 'react/compiler-runtime'
+assert.strictEqual(typeof c, 'function', 'c should be a function')
---
+++ 19.0.0/workaround.mjs
@@ -0,0 +1,5 @@
+import assert from 'node:assert'
+
+import pkg from 'react/compiler-runtime'
+const { c } = pkg
+assert.strictEqual(typeof c, 'function', 'c should be a function')
